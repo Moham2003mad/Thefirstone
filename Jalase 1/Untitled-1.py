5b433716329c59fa2312a4cc6ff4ac95eca26cc9
moham2003mad <msoltanshahi82@gmail.com>
2023-08-04 09:30:47 +0330

    first...

---
+++ Jalase 1/Untitled-1.py
@@ -0,0 +1,52 @@
+# print("Mohammad Soltanshahi")
+# print("Comment in Python")
+# Name = "Mohammad"
+# Family="Soltanshahi"
+# print(Name)
+# print(Family)
+# print(Name,Family)
+# age = 20*365
+# print(age)
+# years=20
+# month=4
+# days=22
+# mydays=years*365 + month*30+ days
+# print(mydays)
+# inforamation=input("Please inter your name :")
+# print( "your name is :",inforamation)
+# age =29;
+# name="ahmad"
+# bol=True;
+# salary=5.500
+# print(type(name))
+# print(type(age))
+# print(type(bol))
+# print(type(salary))
+# print(id(name))
+# year= int(input("please inter your year :"))
+# month= int(input("please inter your month :"))
+# days= int(input("please inter your days :"))
+# print(year, "years and" ,month ,"months and", days ,"days")
+# d=year*365 + months*30 + days
+# print(f"you have{d} days of your life ")
+# a=float(input("inter a number"))
+# b=float(input("inter a number"))
+# c=float(input("inter a number"))
+# x1=(-b+(b**2-4*a*c)**0.5)/2*a
+# x2=(-b-(b**2-4*a*c)**0.5)/2*a
+# print(x1 ,"and", x2)
+# import math
+# from math import sin,pi
+# print(math.sin())
+import math
+v0=float(input('inter the V : '))
+a=float(input('inter the a : '))
+g=10
+R=(v0**2*(math.sin(math.radians(2*a))))/g
+print(R)
+
+
+
+
+
+
